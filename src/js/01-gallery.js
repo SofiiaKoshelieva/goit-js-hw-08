defb4b4c4da1496ae7eb9140e8096e967189fb72
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-01-28 13:38:55 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryEl.addEventListener("click", onGalleryClick);
+function createImgCardsMarkup(galleryItems){
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return   `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    }).join('');
+};
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    if(!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    openLightbox();
+};
+function openLightbox() {
+    let lightbox = new SimpleLightbox('.gallery a',
+        {
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'top',
+            captionDelay: 900,
+            enableKeyboard: true,
+            maxZoom: 0,
+            fadeSpeed: 700,
+        });
+    lightbox.open();
+};
+
 // Change code below this line
 
 console.log(galleryItems);
