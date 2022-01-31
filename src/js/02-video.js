baa24d010e3ec978eacebbea58b9748116b3e0cb
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-01-31 12:31:40 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+player.on('timeupdate', throttle(playerOn, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function() {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});;
+function playerOn(data) {
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+}
